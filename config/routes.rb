c31602c83fca7ba53f48eace58ee6b5641996c71
Francisco Barreto <franciscoantunesbarreto1997@gmail.com>
2018-07-31 18:06:10 +0100

    Added the controllers for the flats,bookings and categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'bookings/index'
+  get 'bookings/show'
+  get 'bookings/new'
+  get 'bookings/create'
+  get 'bookings/edit'
+  get 'bookings/update'
+  get 'bookings/destroy'
+  get 'flats/index'
+  get 'flats/show'
+  get 'flats/new'
+  get 'flats/create'
+  get 'flats/edit'
+  get 'flats/update'
+  get 'flats/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
