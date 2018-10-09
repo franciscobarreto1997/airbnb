059c7a2106fa2231ac51193922f6b3a92e3afd1e
Francisco Barreto <franciscoantunesbarreto1997@gmail.com>
2018-10-09 13:12:45 +0100

    Changed the margin of the cards, and added a link to the show page of each flat

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :categories, only: [:index, :show]
+  resources :flats, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   get 'bookings/index'
   get 'bookings/show'
   get 'bookings/new'
@@ -7,13 +8,6 @@ Rails.application.routes.draw do
   get 'bookings/edit'
   get 'bookings/update'
   get 'bookings/destroy'
-  get 'flats/index'
-  get 'flats/show'
-  get 'flats/new'
-  get 'flats/create'
-  get 'flats/edit'
-  get 'flats/update'
-  get 'flats/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
