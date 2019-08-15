9712025f5ef980dcf5134e720a7cbe969967c612
Francisco Barreto <franciscoantunesbarreto1997@gmail.com>
2019-08-15 15:37:47 +0100

    added modal login, styled modal login, changed the font on the body,added a helper to login with modal, added users controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
-
+  helper ContentHelper
   def home
     @flats = Flat.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   post 'flats', to: 'flats#create'
 
 
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
